7abae3d6880df9cf5551a04244438a42e1c17cdc
yiqi0110 <lili-rovert@hotmail.com>
2018-08-27 16:37:17 -0600

    this is the working one with no bells or whistles

--- assets/javascript/javaS.js
+++ assets/javascript/javaS.js
@@ -11,72 +11,65 @@ var urlHolder = [];
 // function defintions
 // ===============================================================
 
-var makePicture = function(ratings, data) {
-    for (var k = 0; k < ratings.length; k++){
-        var div = $("<div>");
-        div.addClass(ratings[i]);
-        var p = $("<p class'coloreD'>").text("Ratings: " + ratings);
-        var imgSlot = $("<img class='img-fluid'>");
-        imgSlot.attr('src', data[k].images.fixed_height.url);
-        div.append(p).append(imgSlot);
-        $('#placeToPutThings').prepend(div);
-        console.log(data[k]);
+var makeRatingDiv = function () {
+    for (var k = 0; k < ratings.length; k++) {
+        var pdiv = $("<div class='row'>");
+        var p = $("<p>").text("Ratings for these gifs is: " + ratings[k]);
+        p.attr('id', ratings[k]);
+        pdiv.append(p);
+        $("#placeToPutThings").append(pdiv);
+        var imgDiv = $("<div class='row'>");
+        imgDiv.attr('id', ratings[k] + "-img");
+        $("#placeToPutThings").append(imgDiv);
     };
 };
 
 var creatingButtons = function (catagory) {
     var thing = catagory;
     var button = $("<button>").text(thing);
-    button.addClass("d-inline btn btn-secondary _button").attr("type", "button").attr("data-name", thing);
+    button.addClass("d-inline btn btn-secondary _button").attr("type", "button").attr("data-type", thing);
     $("div#containsButtons").append(button);
-};
-
-var giphyRATINGS = function (array) {
-    if (array === "G") {
-        return 3;
-    }
-    if (array === "PG-13") {
-        return 3;
-    }
-    if (array === "PG") {
-        return 4;
-    }
 };
 
 var searchGIPHY = function (search) {
     // use this when calling the search thing in a for loop thang
-    var arr = urlHolder;
-    for (var i = 0; i < ratings.length; i++) {
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=kBzRKprFn5QACjSiqR9QVm2yFNw4kLNP&q=" + search + "&limit=" + giphyRATINGS(ratings[i]) + "&offset=0&rating=" + ratings[i] + "&lang=en";
-        arr.push(queryURL);
-    }
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=kBzRKprFn5QACjSiqR9QVm2yFNw4kLNP&q=" + search + "&limit=10&offset=0&lang=en";
 
-    for (var i = 0; i < ratings.length; i++) {
-        // I guess you need to wrap this around an annonomous function, dont know why but you do haha
-        var annonomous = function () {
-            var currentUrl = arr[i];
-            $.ajax({
-                url: currentUrl,
-                method: "GET"
-            }).then(function (response) {
-                var data = response.data;
-                console.log(data);
-                var div4Img = $("<div class'"+data[i].rating+" row'>");
-                var p = $("<p>").text("Rating: " + data[i].rating);
-                for (var b = 0; b < ratings.length; b++) {
-                    console.log('this many times '+ b);
-                    var image = $("<img class='"+data[b].rating+"'>");
-                    image.attr('src', data[b].images.fixed_height.url);
-                    $(div4Img).prepend(image);
-                    console.log("this should be doing 3 times");
-                };
-                console.log("this should have happend three times");
-                $('#placeToPutThings').prepend(div4Img);
-                // makePicture(ratings, data);
-            });
+    // I guess you need to wrap this around an annonomous function, dont know why but you do haha
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        var data = response.data;
+        console.log(data);
+        for (var i = 0; i < data.length; i++) {
+            var imG = $("<img>");
+            console.log(data[i].rating);
+            var stillIMG = data[i].images.fixed_width_still.url;
+            var animatedIMG = data[i].images.fixed_width.url;
+            imG.attr('src', stillIMG).attr('data-still', stillIMG).attr('data-animate', animatedIMG).attr('data-state', 'still');
+            imG.addClass('searchedImg');
+            if (data[i].rating === 'g') {
+                $("div#G-img").append(imG);
+            } else if (data[i].rating === 'pg') {
+                $("div#PG-img").append(imG);
+            } else {
+                $("div#PG-13-img").append(imG);
+            };
         };
-        annonomous();
-    };
+        if ($("div#G-img:empty").length) {
+            $("p#G").text("no gifs for G ratings");
+            console.log("working");
+        };
+        if ($("div#PG-img:empty").length) {
+            $("p#PG").text("no gifs for PG ratings");
+            console.log("working");
+        };
+        if ($("div#PG-13-img:empty").length) {
+            $("p#PG-13").text("no gifs for PG-13 ratings");
+            console.log("working");
+        };
+    });
 };
 
 // function calls (if any (initaize stuff))
@@ -91,20 +84,27 @@ $("#searchButton").on("click", function (event) {
     // prevent form from submitting
     event.preventDefault();
     var userInput = $("#searchBar").val().trim();
-
-    if (typeof userInput === "string") {
-        console.log("this is working");
-        creatingButtons(userInput);
-        $("#searchBar").val("");
-    } else {
-        console.log("You did not type in a word... rubbish");
-    }
+    creatingButtons(userInput);
+    $("#searchBar").val("");
 });
 
 $(document.body).on("click", "._button", function () {
     $("#placeToPutThings").empty();
+    makeRatingDiv();
     // have something to remove the old gifs
-    var thisButton = $(this).attr("data-name");
-    console.log(thisButton);
+    var thisButton = $(this).attr("data-type");
     searchGIPHY(thisButton);
+    console.log(thisButton);
+});
+
+$(document.body).on("click", ".searchedImg", function(){
+    var gifState = $(this).attr("data-state");
+    if (gifState == "still"){
+        $(this).attr("src", $(this).data("animate"));
+        $(this).attr("data-state", "animated");
+    } else {
+        $(this).attr("src", $(this).data("still"));
+        $(this).attr("data-state", "stilled");
+    }
+
 });
